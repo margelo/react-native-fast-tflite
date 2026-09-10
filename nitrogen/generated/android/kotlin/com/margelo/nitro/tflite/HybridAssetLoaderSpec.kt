cf276b9e9e53bc9ee7072a25dda7009aae4a0272
Patrick Kabwe <patrickckabwe@gmail.com>
2026-09-10 20:39:16 +0200

    chore: upgrade react-native-nitro-modules

--- nitrogen/generated/android/kotlin/com/margelo/nitro/tflite/HybridAssetLoaderSpec.kt
+++ nitrogen/generated/android/kotlin/com/margelo/nitro/tflite/HybridAssetLoaderSpec.kt
@@ -10,6 +10,7 @@ package com.margelo.nitro.tflite
 import androidx.annotation.Keep
 import com.facebook.jni.HybridData
 import com.facebook.proguard.annotations.DoNotStrip
+import dalvik.annotation.optimization.FastNative
 import com.margelo.nitro.core.ArrayBuffer
 import com.margelo.nitro.core.Promise
 import com.margelo.nitro.core.HybridObject
@@ -44,6 +45,7 @@ abstract class HybridAssetLoaderSpec: HybridObject() {
   @Keep
   protected open class CxxPart(javaPart: HybridAssetLoaderSpec): HybridObject.CxxPart(javaPart) {
     // C++ JHybridAssetLoaderSpec::CxxPart::initHybrid(...)
+    @FastNative
     external override fun initHybrid(): HybridData
   }
   override fun createCxxPart(): CxxPart {
